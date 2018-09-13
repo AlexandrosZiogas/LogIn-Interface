becb0601274dce68230413e8477deeca245c05e9
mouxa <alzitv00@gmail.com>
2018-09-13 14:01:46 +0300

    C#

---
+++ LogIn/frmCreate.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace LogIn
+{
+    partial class frmCreate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(133, 38);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(240, 20);
+            this.txtUsername.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(133, 88);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(240, 20);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.btnCreate.Location = new System.Drawing.Point(298, 171);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 30);
+            this.btnCreate.TabIndex = 4;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click_1);
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.lblPassword.Location = new System.Drawing.Point(-1, 87);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(88, 18);
+            this.lblPassword.TabIndex = 7;
+            this.lblPassword.Text = "Password:";
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.lblUsername.Location = new System.Drawing.Point(-1, 40);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(104, 18);
+            this.lblUsername.TabIndex = 6;
+            this.lblUsername.Text = "Usernmame:";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.lblEmail.Location = new System.Drawing.Point(32, 136);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(55, 18);
+            this.lblEmail.TabIndex = 8;
+            this.lblEmail.Text = "Email:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(133, 136);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(240, 20);
+            this.txtEmail.TabIndex = 3;
+            // 
+            // frmCreate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 208);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Name = "frmCreate";
+            this.Text = "frmCreate";
+            this.Load += new System.EventHandler(this.frmCreate_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtEmail;
+    }
+}
---
+++ LogIn/frmCreate.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Odbc;
+using System.Security.Cryptography;
+using System.IO;
+
+
+namespace LogIn
+{
+    public partial class frmCreate : Form
+    {
+        string SQL;
+        OdbcConnection cn = new OdbcConnection();
+        OdbcDataAdapter da;
+        OdbcCommand cmd = new OdbcCommand();
+        DataTable dt = new DataTable();
+        
+        public frmCreate()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click_1(object sender, EventArgs e)
+        {
+           
+            CreateUser();
+        }
+        
+        private void SetPassword(string user, string userPassword)
+        { 
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+            string email = txtEmail.Text;
+            string salt = MyCrypto.getSalt();
+            string HashPassword = MyCrypto.CalcSaltedPass(salt, password);
+            SQL = "UPDATE DATA SET saltedpassword=" + HashPassword + "WHERE username = " + username + "; ";
+            if (IsValidEmail(email) == true)
+            {
+                SQL = "INSERT INTO mylogin.data(username,salt,saltedpassword,email) VALUES('" + username + "','" + salt + "','" + HashPassword + "','" + email + "');";
+            }
+            else
+                MessageBox.Show("Invalid email address");
+            
+        }
+
+        private void CreateUser()
+        {
+            SetPassword(txtUsername.Text, txtPassword.Text);
+            try
+            {
+                if (txtUsername.Text != "" && txtPassword.Text != "" && txtEmail.Text!="")
+                {
+                    da = new OdbcDataAdapter();
+                    da.SelectCommand = new OdbcCommand(SQL,cn);
+                    OdbcCommandBuilder builder = new OdbcCommandBuilder(da);
+                    da.Fill(dt);
+                    MessageBox.Show("User Created");
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Username or Password or Email field is empty");
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "oops!",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+                this.Close();
+            }
+
+        }
+
+        public bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void frmCreate_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                cn.ConnectionString = "Driver={MySQL ODBC 5.3 UNICODE Driver};Server=localhost;Database=mylogin;" +
+               "User=root;Password=;Option=3;";
+                cn.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+                this.Close();
+            }
+
+        }
+
+    }
+}
