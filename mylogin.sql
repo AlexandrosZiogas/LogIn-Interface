4f1dccf1f758950fecb9dd63eb6e2cbcb71e56fc
mouxa <alzitv00@gmail.com>
2018-09-13 13:44:52 +0300

    Database

---
+++ mylogin.sql
@@ -0,0 +1,37 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 10.1.31-MariaDB : Database - mylogin
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`mylogin` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `mylogin`;
+
+/*Table structure for table `data` */
+
+DROP TABLE IF EXISTS `data`;
+
+CREATE TABLE `data` (
+  `username` varchar(20) DEFAULT NULL,
+  `salt` varchar(100) DEFAULT NULL,
+  `saltedpassword` varchar(100) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `data` */
+
+insert  into `data`(`username`,`salt`,`saltedpassword`,`email`) values ('test','j48kockYbAfVyIIMpO9WU7wiVSqrsojxoL9bYmSC62I=','560170a9d23c0b39d4eccb3326de0a879768141934ec56fa924a65a6b0a8dfcc','test@email.com');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
